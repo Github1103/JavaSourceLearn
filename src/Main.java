7be07fa9e3b554afbcb8dd6ebf5f23f7f7fce2fe
= <syrthanfeb@163.com>
2021-11-09 22:02:28 +0800

    environment build

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Description //TODO
+ * @Data 21:18 2021/11/9
+ * @Author zhangjunrong
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO
+    }
+}
---
+++ src/test/hashMap.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description //hashMap查看
+ * @Data 21:44 2021/11/9
+ * @Author zhangjunrong
+ */
+public class hashMap {
+    public static void main(String[] args) {
+        Map<String, Double> hashMap = new HashMap<>();
+
+        hashMap.put("k1",0.1);
+        hashMap.put("k2",0.2);
+        hashMap.put("k3",0.3);
+        hashMap.put("k4",0.4);
+    }
+}
